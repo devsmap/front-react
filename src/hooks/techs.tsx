a4cfeb59808e297170d4f0a898df446d7644edf8
Felipe Chagas <felip.uece@gmail.com>
2021-08-31 21:41:35 -0300

    :sparkles: Adding techs api call

--- src/hooks/techs.tsx
+++ src/hooks/techs.tsx
@@ -1,7 +1,17 @@
 import React, { createContext, useCallback, useContext, useState } from 'react';
 
-// import api from '../services/api';
-import api from '../services/fake/api';
+import api from '../services/api';
+
+import img1 from '../assets/techs/csharp.png';
+import img2 from '../assets/techs/java.png';
+import img3 from '../assets/techs/php.png';
+import img4 from '../assets/techs/python.png';
+import img5 from '../assets/techs/react.png';
+import img6 from '../assets/techs/ruby.png';
+import img7 from '../assets/techs/uiux.png';
+import img8 from '../assets/techs/qa.png';
+import img9 from '../assets/techs/temp1.png';
+import img10 from '../assets/techs/temp2.png';
 
 interface Tech {
   id: number;
@@ -20,14 +30,71 @@ type TechsContextData = {
 
 const TechsContext = createContext<TechsContextData>({} as TechsContextData);
 
+const tempMiddleware = (rawData: Tech[]): TechsState =>
+  rawData.map((value: Tech) => {
+    let image: string;
+
+    switch (value.name) {
+      case 'C#':
+        image = img1;
+        break;
+
+      case 'Java':
+        image = img2;
+        break;
+
+      case 'PHP':
+        image = img3;
+        break;
+
+      case 'Python':
+        image = img4;
+        break;
+
+      case 'React':
+        image = img5;
+        break;
+
+      case 'Ruby on Rails':
+        image = img6;
+        break;
+
+      case 'UX/UI':
+        image = img7;
+        break;
+
+      case 'QA':
+        image = img8;
+        break;
+
+      case 'DevOps':
+        image = img9;
+        break;
+
+      case 'Quality Assurance':
+        image = img10;
+        break;
+
+      default:
+        image = img1;
+    }
+
+    return {
+      ...value,
+      logo: image,
+    };
+  });
+
 const TechsProvider: React.FC = ({ children }) => {
   const [techs, setTechs] = useState<TechsState>([]);
   const [selectedTech, setSelectedTech] = useState<number>(0);
 
   const fetchTechs = useCallback(async () => {
-    const response = await api.get('category');
+    const response = await api.get('categories');
 
-    const techsData: TechsState = [...response.data];
+    const techsDataRaw: TechsState = [...response.data.data];
+
+    const techsData = tempMiddleware(techsDataRaw);
 
     localStorage.setItem('@DevsMap:techs', JSON.stringify(techsData));
 
